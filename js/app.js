27a700489c80bf202ba149920a64e483193ccf05
Alexander <104426761+nemirovka28@users.noreply.github.com>
2025-09-27 23:25:20 +0300

    d
    d

---
+++ js/app.js
@@ -0,0 +1,313 @@
+// Template Name: Homely
+// Template URL: https://techpedia.co.uk/template/homely
+// Description: Homely - Real Estate HTML5 Template
+// Version: 1.0.0
+(function (window, document, $, undefined) {
+  "use strict";
+  // ==========================================================
+  // Detect mobile device and add class "is-mobile" to </body>
+  // ==========================================================
+
+  // Detect mobile device (Do not remove!!!)
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Nokia|Opera Mini/i.test(navigator.userAgent) ? true : false;
+
+  // Add class "is-mobile" to </body>
+  if (isMobile) {
+    $("body").addClass("is-mobile");
+  }
+
+  var Init = {
+    i: function (e) {
+      Init.s();
+      Init.methods();
+    },
+    s: function (e) {
+      (this._window = $(window)),
+        (this._document = $(document)),
+        (this._body = $("body")),
+        (this._html = $("html"));
+    },
+    methods: function (e) {
+      Init.w();
+    
+      Init.dropdown();
+      Init.intializeSlick();
+    },
+    w: function (e) {
+      this._window.on("load", Init.l).on("scroll", Init.res);
+    },
+    BackToTop: function () {
+      var btn = $("#backto-top");
+      $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 300) {
+          btn.addClass("show");
+        } else {
+          btn.removeClass("show");
+        }
+      });
+      btn.on("click", function (e) {
+        e.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: 0,
+          },
+          "300"
+        );
+      });
+    },
+    
+    dropdown: function () {
+      const selectedAll = document.querySelectorAll(".wrapper-dropdown");
+
+      selectedAll.forEach((selected) => {
+        const optionsContainer = selected.children[2];
+        const optionsList = selected.querySelectorAll(
+          "div.wrapper-dropdown li"
+        );
+
+        selected.addEventListener("click", () => {
+          let arrow = selected.children[1];
+
+          if (selected.classList.contains("active")) {
+            handleDropdown(selected, arrow, false);
+          } else {
+            let currentActive = document.querySelector(
+              ".wrapper-dropdown.active"
+            );
+
+            if (currentActive) {
+              let anotherArrow = currentActive.children[1];
+              handleDropdown(currentActive, anotherArrow, false);
+            }
+
+            handleDropdown(selected, arrow, true);
+          }
+        });
+
+        // update the display of the dropdown
+        for (let o of optionsList) {
+          o.addEventListener("click", () => {
+            selected.querySelector(".selected-display").innerHTML = o.innerHTML;
+          });
+        }
+      });
+
+      // check if anything else ofther than the dropdown is clicked
+      window.addEventListener("click", function (e) {
+        if (e.target.closest(".wrapper-dropdown") === null) {
+          closeAllDropdowns();
+        }
+      });
+
+      // close all the dropdowns
+      function closeAllDropdowns() {
+        const selectedAll = document.querySelectorAll(".wrapper-dropdown");
+        selectedAll.forEach((selected) => {
+          const optionsContainer = selected.children[2];
+          let arrow = selected.children[1];
+
+          handleDropdown(selected, arrow, false);
+        });
+      }
+
+      // open all the dropdowns
+      function handleDropdown(dropdown, arrow, open) {
+        if (open) {
+          arrow.classList.add("rotated");
+          dropdown.classList.add("active");
+        } else {
+          arrow.classList.remove("rotated");
+          dropdown.classList.remove("active");
+        }
+      }
+    },
+  
+  
+    intializeSlick: function (e) {
+      if ($(".category-slider").length) {
+        $(".category-slider").slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          centerPadding: '0px',
+          centerMode: true,
+          autoplay: true,
+          dots: false,
+          cssEase: 'linear',
+          autoplaySpeed: 1500,
+          responsive: [
+            {
+              breakpoint: 1199,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 492,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+          ],
+        });
+      }
+ 
+      if ($(".testimonial-slider").length) {
+        $(".testimonial-slider").slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          centerPadding: '0px',
+          centerMode: true,
+          autoplay: true,
+          dots: false,
+          cssEase: 'linear',
+          autoplaySpeed: 2000,
+          responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              },
+            }
+          ],
+        });
+      }
+      if ($(".team-slider").length) {
+        $(".team-slider").slick({
+          infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: false,
+          centerPadding: '0px',
+          centerMode: true,
+          autoplay: true,
+          dots: false,
+          cssEase: 'linear',
+          autoplaySpeed: 2000,
+          responsive: [
+            {
+              breakpoint: 1199,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              },
+            }
+          ],
+        });
+      }
+      if ($(".house-detail-slider").length) {
+        $(".house-detail-slider").slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          fade: true,
+          asNavFor: ".house-detail-slider-nav",
+        });
+      }
+      if ($(".house-detail-slider-nav").length) {
+        $(".house-detail-slider-nav").slick({
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          asNavFor: ".house-detail-slider",
+          dots: false,
+          arrows: false,
+          centerMode: false,
+          focusOnSelect: true,
+          responsive: [
+            {
+              breakpoint: 990,
+              settings: {
+                arrows: false,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                slidesToShow: 2,
+              },
+            },
+          ],
+        });
+      }
+      if ($(".logo-slider").length) {
+        $(".logo-slider").slick({
+          slidesToShow: 4,
+          arrows: false,
+          dots: false,
+          infinite: true,
+          autoplay: true,
+          cssEase: "linear",
+          autoplaySpeed: 0,
+          speed: 6000,
+          pauseOnFocus: false,
+          pauseOnHover: false,
+          responsive: [
+            {
+              breakpoint: 1399,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 580,
+              settings: {
+                slidesToShow: 1,
+              },
+            }
+          ],
+        });
+      }
+
+    },
+
+  }
+  Init.i();
+
+
+
+})(window, document, jQuery);
